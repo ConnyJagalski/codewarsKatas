2f820ef2e23fb5166c715fe983c65405ab1d9f3d
Conny Jagalski <coni.jaga@gmail.com>
2023-09-14 11:21:05 +0200

    feat: added kata Grasshopper - Personalized Message

---
+++ grasshopper-personalizedMessage/script.js
@@ -0,0 +1,10 @@
+function greet (name, owner) {
+  if (name === owner) {
+    return "Hello boss"
+  } else {
+    return "Hello guest"
+  }
+}
+
+console.log(greet('Daniel', 'Daniel'), 'Hello boss');
+console.log(greet('Greg', 'Daniel'), 'Hello guest');
