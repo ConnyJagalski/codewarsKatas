79654d0628439ac7c37bb212c40a8813c2b807d5
Conny Jagalski <coni.jaga@gmail.com>
2023-09-17 12:06:51 +0200

    feat: added kata Total Amount of Points

---
+++ totalAmountOfPoints/script.js
@@ -0,0 +1,21 @@
+function points(games) {
+  let result = 0;
+  
+  for (let index of games) {
+    let [x, y] = index.split(':').map(Number);
+    
+    if (x > y) {
+      result += 3;
+    } else if (x === y) {
+      result += 1;
+    }
+  }
+  
+  return result;
+}
+
+console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30);
+console.log(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]) , 10);
+console.log(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]) , 0);
+console.log(points(["1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"]) , 15);
+console.log(points(["1:0","2:0","3:0","4:4","2:2","3:3","1:4","2:3","2:4","3:4"]) , 12);
