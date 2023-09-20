af284d157bde7c58e5ad62b82a2c1617dc1c2a5e
Conny Jagalski <coni.jaga@gmail.com>
2023-09-20 08:38:40 +0200

    feat: added kata The highest profit wins!

---
+++ theHighestProfitWins!/script.js
@@ -0,0 +1,13 @@
+function minMax(arr){
+  let result = [];
+  
+  result.push(Math.min(...arr));
+  result.push(Math.max(...arr));
+  
+  return result;
+}
+
+
+console.log(minMax([1, 2, 3, 4, 5]), "should return", [1, 5]);
+console.log(minMax([2334454, 5]), "should return", [5, 2334454]);
+console.log(minMax([5]), "should return", [5, 5]);
