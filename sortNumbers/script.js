ed7e0413a2aaaf10669b280f3ea367221b3a3349
Conny Jagalski <coni.jaga@gmail.com>
2023-10-14 12:04:39 +0200

    feat: added kata Sort Numbers

---
+++ sortNumbers/script.js
@@ -0,0 +1,15 @@
+function solution(nums) {
+  if (nums === null) {
+    return [];
+  } else {
+    return nums.sort((a, b) => {
+      return a - b;
+    });
+  }
+}
+
+console.log(solution([1, 2, 3, 10, 5]), [1, 2, 3, 5, 10]);
+console.log(solution(null), []);
+console.log(solution([]), []);
+console.log(solution([20, 2, 10]), [2, 10, 20]);
+console.log(solution([2, 20, 10]), [2, 10, 20]);
