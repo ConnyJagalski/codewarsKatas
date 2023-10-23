3db5de08b2780d1d51184449a61198fb663c05e3
Conny Jagalski <coni.jaga@gmail.com>
2023-10-23 15:01:33 +0200

    feat: added kata What's the real floor?

---
+++ whatsTheRealFloor/script.js
@@ -0,0 +1,8 @@
+function getRealFloor(n) {
+  return n < 1 ? n : n < 13 ? n - 1 : n - 2;
+}
+
+console.log(getRealFloor(1) == 0);
+console.log(getRealFloor(5) == 4);
+console.log(getRealFloor(15) == 13);
+console.log(getRealFloor(-2) == -2);
