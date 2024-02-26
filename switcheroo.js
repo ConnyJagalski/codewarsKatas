f86c0dbce0dfb23747277b8e180967235a37c877
Conny Jagalski <coni.jaga@gmail.com>
2024-02-26 13:32:33 +0100

    feat: added kata Switcheroo

---
+++ switcheroo.js
@@ -0,0 +1,23 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x) {
+  return x
+    .split("")
+    .map((element) =>
+      element === "a"
+        ? (element = "b")
+        : element === "b"
+        ? (element = "a")
+        : element
+    )
+    .join("");
+}
+
+console.log(switcheroo("abc") === "bac");
+console.log(switcheroo("aaabcccbaaa") === "bbbacccabbb");
+console.log(switcheroo("ccccc") === "ccccc");
