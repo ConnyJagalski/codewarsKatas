845018ea95457f7e1f9e72a6fc8dc271b08cf8e3
Conny Jagalski <coni.jaga@gmail.com>
2024-03-09 10:44:38 +0100

    feat: added kata Kata Example Twist

---
+++ kataExampleTwist.js
@@ -0,0 +1,9 @@
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+const websites = "codewars ".repeat(1000).trim().split(" ");
+
+console.log(websites.length);
+console.log(websites);
+console.log(websites.reverse());
